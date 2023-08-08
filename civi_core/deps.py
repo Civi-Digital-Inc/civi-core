88314cd13272baa18742a10a504be596fbe16de9
gaytomycode <hi@gaytomycode.com>
2023-08-08 15:11:46 +0300

    feat(deps): Added role to the token decoder.

--- civi_core/deps.py
+++ civi_core/deps.py
@@ -38,7 +38,7 @@ def getconn():
         db=os.getenv("DB_NAME"),
     )
     return conn
-    
+
 __engine = create_engine("postgresql+pg8000://", creator=getconn)
 __SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=__engine)
 
@@ -66,6 +66,7 @@ class TokenData(BaseModel):
 
     id: Optional[int] = None
     email: Optional[str] = None
+    role: Optional[List[str]] = None
     token: Optional[str] = None
 
 
@@ -89,11 +90,12 @@ async def get_current_identity(
             algorithms=[settings.ALGORITHM],
             options={"verify_aud": False},
         )
-        identity_id: int = payload.get("id", "")
-        email: str = payload.get("email", "")
+        identity_id: int = payload.get('id', '')
+        email: str = payload.get('email', '')
+        role: List[str] = literal_eval(payload.get('role', '[]'))
         if identity_id is None or email is None:
             raise __credentials_exception
-        token_data = TokenData(id=identity_id, email=email, token=token)
+        token_data = TokenData(id=identity_id, email=email, token=token, role=role)
     except JWTError:
         raise __credentials_exception
     return token_data
